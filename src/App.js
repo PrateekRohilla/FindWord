99777dacc56ada5e2fc3e19b81d4d640e7a41d24
prateekrohilla <prateek98rohilla@gmail.com>
2022-02-19 16:30:39 +0530

    api addede & run  test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [word, setWord] = useState("")
+  const [meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async() => {
+    try {
+      const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/plane");
+
+      setMeanings(data.data);
+
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log(meanings);
+
+  useEffect(() => {
+    
+    dictionaryApi();
+
+  }, [])
+  
+
   return (
+
     <div className="App">
-      Hello world
     </div>
   );
 }
